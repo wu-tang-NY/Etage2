1d0defa2341df86c0a5fb37489a2566b7ab25b52
andriikapustin <andrii.kapustin@extrawest.com>
2022-10-23 21:53:52 +0200

    added server

---
+++ server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+const publicPath = path.join(__dirname, '..', 'dist');
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}!`);
+});
